3b9fca902c1a08d548758a06c0bf1f0ec52d11dd
Don <donavan.olsen@gmail.com>
2022-07-13 16:57:14 -0700

    Setup initial project structure

---
+++ src/UI/Features/DayDetails/ViewDay.razor.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace DateBlazor.UI.Features.DayDetails
+{
+    public partial class ViewDay : ComponentBase
+    {
+        [Parameter]
+        public string? DateString { get; set; }
+
+        string? _errorMessage;
+        DateTime _date;
+
+        protected override async Task OnInitializedAsync()
+        {
+            bool isDate = DateTime.TryParse(DateString, out _date);
+            if (!isDate)
+            {
+                _errorMessage = "Something went wrong";
+            }
+
+            await base.OnInitializedAsync();
+        }
+    }
+}
---
+++ src/UI/Features/Home/HomePage.razor.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace DateBlazor.UI.Features.Home
+{
+    public partial class HomePage: ComponentBase
+    {
+        string _message = "Hello World";
+    }
+}
